847d0178d128e7c42d399480103b0c9fbb2cb197
Neelesh Chevuri <neelesh.chevuri25@gmail.com>
2025-06-15 13:50:59 -0500

    Using Altstudio instead

--- src/index.ts
+++
@@ -1,6 +0,0 @@
-import { makeSourceHandler } from "sidesource";
-
-export default makeSourceHandler({
-  configURL: "https://raw.githubusercontent.com/cyberneel/cyberneel-sidesource/main/config.json",
-  cacheTime: 300
-});
